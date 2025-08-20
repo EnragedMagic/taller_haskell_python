8f8f29b30c100300a2c7983c8389b659fbadeea8
Johan Galeano <104652500+EnragedMagic@users.noreply.github.com>
2025-08-20 08:01:44 -0500

    Update problem5.py

--- python/problem5.py
+++ python/problem5.py
@@ -1,1 +1,26 @@
+# problem5.py
+IMPUESTOS = 53206000  
+
+def debe_pagar_impuestos(edad: int, ingresos: float) -> bool:
+    """
+    Determina si un usuario debe pagar impuestos.
+    """
+    if edad < 0 or edad > 120:
+        raise ValueError("Edad es valiida.")
+    if ingresos < 0:
+        raise ValueError("Ingresos no pueden ser negativos.")
+    return edad >= 18 and ingresos >= IMPUESTOS
+
+
+if __name__ == "__main__":
+    try:
+        edad = int(input("Ingrese su edad: "))
+        ingresos = float(input("Ingrese el total acumulado de consignaciones anuales: "))
+
+        if debe_pagar_impuestos(edad, ingresos):
+            print("Debe pagar impuestos.")
+        else:
+            print("No debe pagar impuestos.")
+    except ValueError as e:
+        print("Error:", e)
 
