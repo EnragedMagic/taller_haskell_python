1450e9b70742c685dab5efa55f10a339039b35cd
Johan Galeano <104652500+EnragedMagic@users.noreply.github.com>
2025-08-20 07:47:50 -0500

    Update problema3.py

--- python/problema3.py
+++ python/problema3.py
@@ -1,1 +1,19 @@
+# problem3.py
+def dividir(a: float, b: float):
+    """
+    Realiza la división a / b.
+    """
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Error: dividiste por cero"
 
+
+if __name__ == "__main__":
+    try:
+        a = float(input("Numerador: "))
+        b = float(input("Denominador: "))
+        resultado = dividir(a, b)
+        print("Resultado:", resultado)
+    except ValueError:
+        print("Error ingrese numeros validos.")
