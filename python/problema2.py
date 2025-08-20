94c041dcadeda424962fd085c41f88081dbafd76
Johan Galeano <104652500+EnragedMagic@users.noreply.github.com>
2025-08-20 07:44:54 -0500

    Update problema2.py

--- python/problema2.py
+++ python/problema2.py
@@ -1,1 +1,21 @@
+# problem2.py
+def coincide_password(guardada: str, entrada: str) -> bool:
+    """
+    Compara dos contraseñas.
+    
+    """
+    if not entrada:  # si esta vacia
+        return 
+    return guardada.lower() == entrada.lower()
+
+
+if __name__ == "__main__":
+    PASSWORD = "Magic2005$"  # contraseña almacenada
+    
+    entrada = input("Escribe la contraseña: ").strip()
+    
+    if coincide_password(PASSWORD, entrada):
+        print("Contraseña correcta ")
+    else:
+        print("Contraseña incorrecta ")
 
